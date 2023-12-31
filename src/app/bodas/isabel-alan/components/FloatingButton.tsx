98e1ccbe5f7c1b5a5c2ee1d2aa74e102e8c5223d
Gus Garcia <gus16710@gmail.com>
2023-12-31 11:27:27 -0600

    12/31/2023 End of the Year

--- src/app/bodas/isabel-alan/components/FloatingButton.tsx
+++ src/app/bodas/isabel-alan/components/FloatingButton.tsx
@@ -1,4 +1,4 @@
-import { useAnimate } from "framer-motion";
+import { useAnimate, motion } from "framer-motion";
 import { useState, useEffect } from "react";
 import { IoIosArrowUp } from "react-icons/io";
 
@@ -6,12 +6,10 @@ export default function FloatinButton() {
   const [scope, animate] = useAnimate();
 
   useEffect(() => {
-    animate(scope.current, { opacity: 0, scale: 0 }, { duration: 0 });
-
     window.addEventListener("scroll", () => {
       if (
-        document.body.scrollTop > 40 ||
-        document.documentElement.scrollTop > 40
+        document.body.scrollTop > 50 ||
+        document.documentElement.scrollTop > 50
       ) {
         animate(scope.current, {
           scale: 1,
@@ -27,15 +25,16 @@ export default function FloatinButton() {
   }, []);
 
   return (
-    <button
+    <motion.button
       type="button"
       className={`bg-slate-200/50 p-3 rounded-full text-zinc-800 fixed bottom-0 right-0 font-medium shadow-md mb-10 mr-5 transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg z-10`}
       onClick={() => {
         window.scrollTo({ top: 0, behavior: "smooth" });
       }}
       ref={scope}
+      initial={{ opacity: 0, scale: 0 }}
     >
       <IoIosArrowUp />
-    </button>
+    </motion.button>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" style={{ scrollSnapType: "y mandatory" }}>
+    <html lang="en" style={{ scrollSnapType: "y mandatory" }} className="scroll-smooth">
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
