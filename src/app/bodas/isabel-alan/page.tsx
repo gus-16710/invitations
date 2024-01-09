3b04af01e21d0ae9f18c3b2476bbef0a91d4da4f
Gus Garcia <gus16710@gmail.com>
2024-01-09 13:30:14 -0600

    Changing background invitations

--- src/app/bodas/isabel-alan/page.tsx
+++ src/app/bodas/isabel-alan/page.tsx
@@ -14,12 +14,10 @@ export default function Wedding() {
   return (
     <main
       style={{
-        backgroundColor: "#f3f3f3",
-        backgroundImage:
-          "linear-gradient(135deg, #fafafa 25%, transparent 25%), linear-gradient(225deg, #fafafa 25%, transparent 25%), linear-gradient(45deg, #fafafa 25%, transparent 25%), linear-gradient(315deg, #fafafa 25%, #f3f3f3 25%)",
-        backgroundPosition: "40px 0, 40px 0, 0 0, 0 0",
-        backgroundSize: "40px 40px",
-        backgroundRepeat: "repeat",
+        backgroundColor: "#000000",
+        backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 800 400'%3E%3Cdefs%3E%3CradialGradient id='a' cx='396' cy='281' r='514' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23DDDDDC'/%3E%3Cstop offset='1' stop-color='%23000000'/%3E%3C/radialGradient%3E%3ClinearGradient id='b' gradientUnits='userSpaceOnUse' x1='400' y1='148' x2='400' y2='333'%3E%3Cstop offset='0' stop-color='%23FFFFFF' stop-opacity='0'/%3E%3Cstop offset='1' stop-color='%23FFFFFF' stop-opacity='0.5'/%3E%3C/linearGradient%3E%3C/defs%3E%3Crect fill='url(%23a)' width='800' height='400'/%3E%3Cg fill-opacity='0.4'%3E%3Ccircle fill='url(%23b)' cx='267.5' cy='61' r='300'/%3E%3Ccircle fill='url(%23b)' cx='532.5' cy='61' r='300'/%3E%3Ccircle fill='url(%23b)' cx='400' cy='30' r='300'/%3E%3C/g%3E%3C/svg%3E")`,
+        backgroundAttachment: "fixed",
+        backgroundSize: "cover",
       }}
     >
       <div className="max-w-3xl m-auto shadow-large bg-white">
--- src/app/quinces/camila/page.tsx
+++ src/app/quinces/camila/page.tsx
@@ -83,11 +83,11 @@ export default function Fifteen() {
 
   return (
     <main
-      className=""
       style={{
-        backgroundColor: "#fde3ff",
-        backgroundImage:
-          "repeating-radial-gradient( circle at 0 0, transparent 0, #fde3ff 22px ), repeating-linear-gradient( #fafafa55, #fafafa )",
+        backgroundColor: "#99154B",
+        backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 1000'%3E%3Cg %3E%3Ccircle fill='%2399154B' cx='50' cy='0' r='50'/%3E%3Cg fill='%239f2752' %3E%3Ccircle cx='0' cy='50' r='50'/%3E%3Ccircle cx='100' cy='50' r='50'/%3E%3C/g%3E%3Ccircle fill='%23a4345a' cx='50' cy='100' r='50'/%3E%3Cg fill='%23aa4062' %3E%3Ccircle cx='0' cy='150' r='50'/%3E%3Ccircle cx='100' cy='150' r='50'/%3E%3C/g%3E%3Ccircle fill='%23af4c69' cx='50' cy='200' r='50'/%3E%3Cg fill='%23b45771' %3E%3Ccircle cx='0' cy='250' r='50'/%3E%3Ccircle cx='100' cy='250' r='50'/%3E%3C/g%3E%3Ccircle fill='%23ba6179' cx='50' cy='300' r='50'/%3E%3Cg fill='%23bf6c82' %3E%3Ccircle cx='0' cy='350' r='50'/%3E%3Ccircle cx='100' cy='350' r='50'/%3E%3C/g%3E%3Ccircle fill='%23c3768a' cx='50' cy='400' r='50'/%3E%3Cg fill='%23c88192' %3E%3Ccircle cx='0' cy='450' r='50'/%3E%3Ccircle cx='100' cy='450' r='50'/%3E%3C/g%3E%3Ccircle fill='%23cd8b9a' cx='50' cy='500' r='50'/%3E%3Cg fill='%23d195a3' %3E%3Ccircle cx='0' cy='550' r='50'/%3E%3Ccircle cx='100' cy='550' r='50'/%3E%3C/g%3E%3Ccircle fill='%23d59fac' cx='50' cy='600' r='50'/%3E%3Cg fill='%23d9aab4' %3E%3Ccircle cx='0' cy='650' r='50'/%3E%3Ccircle cx='100' cy='650' r='50'/%3E%3C/g%3E%3Ccircle fill='%23deb4bd' cx='50' cy='700' r='50'/%3E%3Cg fill='%23e1bec6' %3E%3Ccircle cx='0' cy='750' r='50'/%3E%3Ccircle cx='100' cy='750' r='50'/%3E%3C/g%3E%3Ccircle fill='%23e5c9cf' cx='50' cy='800' r='50'/%3E%3Cg fill='%23e9d3d8' %3E%3Ccircle cx='0' cy='850' r='50'/%3E%3Ccircle cx='100' cy='850' r='50'/%3E%3C/g%3E%3Ccircle fill='%23ecdee1' cx='50' cy='900' r='50'/%3E%3Cg fill='%23f0e8ea' %3E%3Ccircle cx='0' cy='950' r='50'/%3E%3Ccircle cx='100' cy='950' r='50'/%3E%3C/g%3E%3Ccircle fill='%23F3F3F3' cx='50' cy='1000' r='50'/%3E%3C/g%3E%3C/svg%3E")`,
+        backgroundAttachment: "fixed",
+        backgroundSize: "contain",
       }}
     >
       {open ? (
@@ -102,7 +102,7 @@ export default function Fifteen() {
           <AudioControl />
         </div>
       ) : (
-        <section className="h-screen bg-[url('/img/quinces/camila/background-header.jpg')] bg-center bg-cover" />
+        <div className="h-screen bg-[url('/img/quinces/camila/background-header.jpg')] bg-center bg-cover" />
       )}
 
       <OpeningModal
