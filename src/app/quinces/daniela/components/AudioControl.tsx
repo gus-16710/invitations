17e432d6f0ddf1d84022d534de2859db6a0fe669
Gus Garcia <gus16710@gmail.com>
2024-01-12 16:47:36 -0600

    Daniela invitation

--- src/app/quinces/daniela/components/AudioControl.tsx
+++ src/app/quinces/daniela/components/AudioControl.tsx
@@ -18,7 +18,7 @@ export default function AudioControl() {
     <>
       <motion.button
         type="button"
-        className="bg-zinc-900/75 p-3 rounded-full text-white fixed bottom-0 right-0 font-medium shadow-md mb-5 mr-5 transition duration-150 ease-in-out hover:bg-zinc-700/75 hover:shadow-lg focus:bg-zinc-700/75 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-zinc-800/75 active:shadow-lg z-10"
+        className="bg-zinc-900/50 p-3 rounded-full text-white fixed bottom-0 right-0 font-medium shadow-md mb-5 mr-5 transition duration-150 ease-in-out hover:bg-zinc-700/75 hover:shadow-lg focus:bg-zinc-700/75 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-zinc-800/75 active:shadow-lg z-10"
         onClick={() => setIsPlayed(!isPlayed)}
         initial={{ opacity: 0, scale: 0, y: -100 }}
         animate={{ opacity: 1, scale: 1, y: 0 }}
