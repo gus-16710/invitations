539eefe8e56344205a6e5c05275346491f1ea53d
Gustavo <gus16710@gmail.com>
2024-04-02 19:31:42 -0600

    Adding whatsapp link with new celphone number to Ashley invitation

--- src/app/quinces/ashley-michelle/components/Confirm.tsx
+++ src/app/quinces/ashley-michelle/components/Confirm.tsx
@@ -62,7 +62,7 @@ export default function Confirm() {
         <button
           type="button"
           className="flex justify-center items-center gap-2 w-60 mt-3 text-pink-900 bg-white border border-gray-400 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 rounded-full me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 font-medium text-sm px-5 py-2.5 text-center z-20"
-          onClick={() => window.open("https://wa.link/4ohzaj", "_blank")}
+          onClick={() => window.open("https://wa.link/xkhs4b", "_blank")}
         >
           <FaWhatsapp className="text-2xl" />
           Mensaje de Whatsapp
