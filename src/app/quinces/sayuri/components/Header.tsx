432ec778dfc6a8e32cabca3869f3caf0fd37be93
Gustavo <gus16710@gmail.com>
2024-06-28 12:43:08 -0600

    Changing the celebration year

--- src/app/quinces/sayuri/components/Header.tsx
+++ src/app/quinces/sayuri/components/Header.tsx
@@ -108,7 +108,7 @@ export default function Header() {
           initial="hidden"
           whileInView="visible"
         >
-          2023
+          2024
         </motion.p>
       </div>
     </section>
