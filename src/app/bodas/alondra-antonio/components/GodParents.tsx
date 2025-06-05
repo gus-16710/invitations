8e999572cda57f77f9cd6550af1184b1dba18ea1
Gustavo <gus16710@gmail.com>
2025-06-04 18:40:52 -0600

    04/06/2025

--- src/app/bodas/alondra-antonio/components/GodParents.tsx
+++ src/app/bodas/alondra-antonio/components/GodParents.tsx
@@ -20,8 +20,8 @@ const customTheme: FlowbiteCarouselTheme = {
   },
   indicators: {
     active: {
-      off: "bg-gray-800/50 hover:bg-gray-800",
-      on: "bg-gray-800 dark:bg-gray-800",
+      off: "bg-pink-100/50 hover:bg-gray-800",
+      on: "bg-pink-100",
     },
     base: "h-3 w-3 rounded-full",
     wrapper: "absolute bottom-5 left-1/2 flex -translate-x-1/2 space-x-3",
@@ -31,8 +31,8 @@ const customTheme: FlowbiteCarouselTheme = {
     wrapper: "w-full flex-shrink-0 transform cursor-default snap-center",
   },
   control: {
-    base: "inline-flex h-8 w-8 items-center justify-center rounded-full bg-gray-800/10 group-hover:bg-gray-800/30 group-focus:outline-none group-focus:ring-4 group-focus:ring-white dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70 sm:h-10 sm:w-10",
-    icon: "h-5 w-5 text-grat-800 dark:text-gray-800 sm:h-6 sm:w-6",
+    base: "inline-flex h-8 w-8 items-center justify-center rounded-full bg-gray-800/10 group-hover:bg-gray-800/30 group-focus:outline-none group-focus:ring-4 group-focus:ring-white sm:h-10 sm:w-10",
+    icon: "h-5 w-5 text-gray-100 sm:h-6 sm:w-6",
   },
   scrollContainer: {
     base: "flex h-full snap-mandatory overflow-y-hidden overflow-x-scroll scroll-smooth rounded-lg",
@@ -47,7 +47,7 @@ export default function GodParents() {
       style={{ height: "100svh" }}
     >
       <motion.h1
-        className={`${greatVibes.className} text-5xl drop-shadow text-center`}
+        className={`${greatVibes.className} text-5xl drop-shadow text-center text-zinc-300`}
         initial={{ y: -100, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ duration: 1, ease: "easeOut", delay: 0.4 }}
@@ -56,21 +56,21 @@ export default function GodParents() {
       </motion.h1>
       <Flowbite>
         <Carousel theme={customTheme}>
-          <div className="flex h-full items-center justify-center pb-10 text-gray-800 flex-col px-5 text-center">
+          <div className="flex h-full items-center justify-center pb-10 flex-col px-5 text-center">
             <motion.div
               initial={{ scale: 0, opacity: 0 }}
               whileInView={{ scale: 1, opacity: 1 }}
               transition={{ duration: 1, ease: "easeOut" }}
               className="flex items-center flex-col"
             >
-              <GiDiamondRing className="text-7xl pb-5 text-gray-800/50" />
-              <p className={`${greatVibes.className} text-4xl pb-4`}>Anillos</p>
+              <GiDiamondRing className="text-7xl pb-5 text-zinc-100/50" />
+              <p className={`${greatVibes.className} text-4xl pb-4 text-zinc-300`}>Anillos</p>
             </motion.div>
             <motion.p
-              className={`${playFair.className}`}
+              className={`${playFair.className} text-zinc-300`}
               initial={{ scale: 0, opacity: 0 }}
               whileInView={{ scale: 1, opacity: 1 }}
-              transition={{ duration: 1, ease: "easeOut" }}
+              transition={{ duration: 1, ease: "easeOut" }}              
             >
               Mariela Macias Rincon <br />&<br /> Ignacio López Lozano
             </motion.p>
@@ -82,13 +82,13 @@ export default function GodParents() {
               transition={{ duration: 1, ease: "easeOut" }}
               className="flex items-center flex-col"
             >
-              <IoIosHeartEmpty className="text-7xl pb-5 text-gray-800/50" />
-              <p className={`${greatVibes.className} text-4xl pb-4`}>
+              <IoIosHeartEmpty className="text-7xl pb-5 text-zinc-100/50" />
+              <p className={`${greatVibes.className} text-4xl pb-4 text-zinc-300`}>
                 Velación
               </p>
             </motion.div>
             <motion.p
-              className={`${playFair.className}`}
+              className={`${playFair.className} text-zinc-300`}
               initial={{ scale: 0, opacity: 0 }}
               whileInView={{ scale: 1, opacity: 1 }}
               transition={{ duration: 1, ease: "easeOut" }}
@@ -96,18 +96,18 @@ export default function GodParents() {
               Leticia Barragan Saldaña <br />&<br /> Juan Pablo Robles Castillo
             </motion.p>
           </div>
-          <div className="flex h-full items-center justify-center text-gray-800 flex-col px-5 text-center">
+          <div className="flex h-full items-center justify-center flex-col px-5 text-center">
             <motion.div
               initial={{ scale: 0, opacity: 0 }}
               whileInView={{ scale: 1, opacity: 1 }}
               transition={{ duration: 1, ease: "easeOut" }}
               className="flex items-center flex-col"
             >
-              <RiLinksLine className="text-7xl pb-5 text-gray-800/50" />
-              <p className={`${greatVibes.className} text-4xl pb-4`}>Lazo</p>
+              <RiLinksLine className="text-7xl pb-5 text-zinc-100/50" />
+              <p className={`${greatVibes.className} text-4xl pb-4 text-zinc-300`}>Lazo</p>
             </motion.div>
             <motion.p
-              className={`${playFair.className}`}
+              className={`${playFair.className} text-zinc-300`}
               initial={{ scale: 0, opacity: 0 }}
               whileInView={{ scale: 1, opacity: 1 }}
               transition={{ duration: 1, ease: "easeOut" }}
--- src/app/bodas/alondra-antonio/components/Itinerary.tsx
+++ src/app/bodas/alondra-antonio/components/Itinerary.tsx
@@ -6,28 +6,24 @@ const playFair = Playfair_Display({ subsets: ["latin"], weight: "400" });
 
 const itinerary = [
   {
-    time: "18:00 Hrs",
-    event: "Ceremonia  Religiosa",
+    time: "12:00 Hrs",
+    event: "Ceremonia Religiosa",
   },
   {
-    time: "19:00 Hrs",
-    event: "Boda Civil",
-  },
-  {
-    time: "20:00 Hrs",
+    time: "15:00 Hrs",
     event: "Recepción",
+  },  
+  {
+    time: "15:30 Hrs",
+    event: "Entrada de Novios",
   },
   {
-    time: "21:00 Hrs",
-    event: "Cena",
+    time: "16:00 Hrs",
+    event: "Brindis",
   },
   {
-    time: "22:00 Hrs",
-    event: "Baile",
-  },
-  {
-    time: "03:00 Hrs",
-    event: "Termino del Evento",
+    time: "16:30 Hrs",
+    event: "Brindis",
   },
 ];
 
@@ -61,7 +57,7 @@ export default function Itinerary() {
       style={{ height: "100svh" }}
     >
       <motion.h1
-        className={`${greatVibes.className} text-5xl drop-shadow mb-10`}
+        className={`${greatVibes.className} text-5xl drop-shadow mb-10 text-zinc-300`}
         initial={{ y: -100, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ duration: 1, ease: "easeOut", delay: 0.3 }}
@@ -70,7 +66,7 @@ export default function Itinerary() {
       </motion.h1>
 
       <motion.ol
-        className="relative border-s border-gray-200 dark:border-gray-700"
+        className="relative border-s border-pink-600"
         initial="hidden"
         whileInView="visible"
         variants={list}
@@ -78,13 +74,13 @@ export default function Itinerary() {
         {itinerary.map((item, index) => (
           <div key={index}>
             <li className="mb-7 ms-4">
-              <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+              <div className="absolute w-3 h-3 bg-pink-600 rounded-full mt-1.5 -start-1.5 border border-pink-700"></div>
               <motion.div variants={element}>
-                <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                <time className="mb-1 text-sm font-normal leading-none text-gray-400 ">
                   {item.time}
                 </time>
                 <h3
-                  className={`${playFair.className} text-base font-semibold text-gray-900 dark:text-white`}
+                  className={`${playFair.className} text-base font-semibold text-zinc-300`}
                 >
                   {item.event}
                 </h3>
