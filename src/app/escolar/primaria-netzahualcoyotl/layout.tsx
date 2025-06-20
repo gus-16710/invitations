306c584c5a9272c414c203d3c42eaa0694cdd516
Gustavo <gus16710@gmail.com>
2025-06-20 12:39:34 -0600

    invitations primaria netzahualcoyotl

--- src/app/escolar/primaria-netzahualcoyotl/layout.tsx
+++ src/app/escolar/primaria-netzahualcoyotl/layout.tsx
@@ -3,11 +3,11 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "🎓 Primaria Netzahualcoyótl | Fin de Cursos 🎓",
   description:
-    "Primaria Netzahualcoyotl “Fin de cursos” de la GENERACIÓN 2019-2025",
+    "Primaria Netzahualcoyótl “Fin de cursos” de la GENERACIÓN 2019-2025",
   openGraph: {
     title: "🎓 Primaria Netzahualcoyótl | Fin de Cursos 🎓",
     description:
-      "Primaria Netzahualcoyotl “Fin de cursos” de la GENERACIÓN 2019-2025",
+      "Primaria Netzahualcoyótl “Fin de cursos” de la GENERACIÓN 2019-2025",
     images: [
       `https://invitaciones.unaideamas.com/img/escolar/primaria-netzahualcoyotl/generacion.jpeg`,
     ],
